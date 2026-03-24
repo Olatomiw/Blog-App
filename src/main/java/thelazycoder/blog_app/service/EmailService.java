068ffc19fc17f75120dce6d63b2d3cea811c7116
DevTom <72770851+Olatomiw@users.noreply.github.com>
2026-03-24 15:00:24 +0100

    mail fcx'

--- src/main/java/thelazycoder/blog_app/service/EmailService.java
+++ src/main/java/thelazycoder/blog_app/service/EmailService.java
@@ -6,20 +6,20 @@ import org.springframework.stereotype.Component;
 
 import java.sql.PreparedStatement;
 
-@Component
-public class EmailService{
-
-    private final JavaMailSender mailSender;
-
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
-    }
-}
+//@Component
+//public class EmailService{
+//
+//    private final JavaMailSender mailSender;
+//
+//    public EmailService(JavaMailSender mailSender) {
+//        this.mailSender = mailSender;
+//    }
+//
+//    public void sendEmail(String to, String subject, String body) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(body);
+//        mailSender.send(message);
+//    }
+//}
