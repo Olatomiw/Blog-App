4711a0b87021e8605e0cf8773b61c280e6a267e4
DevTom <72770851+Olatomiw@users.noreply.github.com>
2026-03-24 11:56:52 +0100

    test flight

--- src/test/java/thelazycoder/blog_app/BlogAppApplicationTests.java
+++ src/test/java/thelazycoder/blog_app/BlogAppApplicationTests.java
@@ -2,15 +2,30 @@ package thelazycoder.blog_app;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import thelazycoder.blog_app.config.CloudinaryService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BlogAppApplication.class)
+@ActiveProfiles("test")
 class BlogAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+    @MockitoBean
+    private JavaMailSender mailSender;
+
+    @MockitoBean
+    private CloudinaryService cloudinaryService; // or whatever you named it
+
+    @MockitoBean
+    private ChatClient chatClient;
 
 }
